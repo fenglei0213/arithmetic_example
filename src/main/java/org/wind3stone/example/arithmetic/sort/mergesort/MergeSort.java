4eed770c7d1890d9b7b876570ca53def7dcde21a
fenglei <fenglei0213@163.com>
2016-07-23 23:01:17 +0800

    m

---
+++ src/main/java/org/wind3stone/example/arithmetic/sort/mergesort/MergeSort.java
@@ -0,0 +1,14 @@
+package org.wind3stone.example.arithmetic.sort.mergesort;
+
+/**
+ * Created by fenglei on 2016/7/23.
+ */
+public class MergeSort {
+
+    public static void main(String[] args) {
+        Integer[] array = {1,2};
+        Integer[] array2 = new Integer[2];
+
+    }
+
+}
---
+++ src/main/java/org/wind3stone/example/arithmetic/sort/quicksort/QuickSort.java
@@ -0,0 +1,7 @@
+package org.wind3stone.example.arithmetic.sort.quicksort;
+
+/**
+ * Created by fenglei on 2016/7/23.
+ */
+public class QuickSort {
+}
